8a3cf1cff89d837272844293a740c1848e8871a1
Jennifer <jenniferlewmunoz@gmail.com>
2023-06-28 09:42:38 -0700

    Added audio for when timer is done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var player: AVAudioPlayer?
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+    
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        header.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed <= totalTime {
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            print(percentageProgress)
+            progressBar.progress = percentageProgress
+            secondsPassed += 1
+        } else {
+            self.header.text = "Done!"
+            timer.invalidate()
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player!.play()
+        }
+    }
+    
+    
 }
+
